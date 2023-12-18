f1b3eb50d74f16d10252fa1c0d640de8dc290c44
gf12\User <sbsgameas@gmail.com>
2023-12-18 21:51:48 +0900

    <23 / 12 / 18 > Update Note 1. Run / 점멸 기능 구현 2. SpawnManager SpawnPoint Area 설치중

--- Assets/CODE/PlayerBle/Movement.cs
+++ Assets/CODE/PlayerBle/Movement.cs
@@ -8,7 +8,6 @@ public class Movement : MonoBehaviour
     [Space]
     [SerializeField] float CharMove_Speed;
     [SerializeField] float Sprint_Speed;
-    [SerializeField] float Force;
 
 
     Rigidbody2D rb;
@@ -27,7 +26,7 @@ public class Movement : MonoBehaviour
     }
     void Start()
     {
-
+        curSpintTime = maxSpintTime;
     }
     private void FixedUpdate()
     {
@@ -40,8 +39,8 @@ public class Movement : MonoBehaviour
         Change_Sclae_Xvalue();
         Animator_Updater();
 
-
-
+        Sprint_Time_Updater();
+        FrontTeleport();
     }
 
 
@@ -82,22 +81,96 @@ public class Movement : MonoBehaviour
         }
     }
 
-    //[SerializeField] float MoveCheak; // 스프린트 작동 시간 제어
-    //private void Sprint_Time_Updater()
-    //{
-    //    if(InputLshift && )
-    //}
+    float MoveCheakX; // 스프린트 작동 시간 제어
+    float MoveCheakY; // 스프린트 작동 시간 제어
+    float CheakMoveFloat; // 스프린트 작동 시간 제어
+    [SerializeField] float curSpintTime;
+    [SerializeField] float maxSpintTime;
+    private void Sprint_Time_Updater()
+    {
+        MoveCheakX = Mathf.Abs(moveVec.x);
+        MoveCheakY = Mathf.Abs(moveVec.y);
+
+        CheakMoveFloat = MoveCheakX + MoveCheakY;
+
+        if (InputLshift && CheakMoveFloat > 0) // 소모
+        {
+            if (curSpintTime > 0)
+            {
+                curSpintTime -= Time.deltaTime;
+            }
+            else if (curSpintTime <= 0)
+            {
+                curSpintTime = 0;
+            }
+        }
+        else // 회복
+        {
+            curSpintTime += Time.deltaTime;
+
+            if (curSpintTime > maxSpintTime)
+            {
+                curSpintTime = maxSpintTime;
+            }
+        }
+    }
+
+    [SerializeField] bool isRun;
     private void Move_Character()
     {
         moveVec = moveVec.normalized;
 
-        if (InputLshift)
+        if (InputLshift == true && curSpintTime > 0)
         {
-            rb.MovePosition(rb.position + moveVec * (CharMove_Speed  + Sprint_Speed ) * Time.deltaTime);
+            isRun = true;
+        }
+        else if (InputLshift == false || curSpintTime <= 0)
+        {
+            isRun = false;
+        }
+
+
+        if (isRun)
+        {
+            rb.MovePosition(rb.position + moveVec * (CharMove_Speed + Sprint_Speed) * Time.deltaTime);
         }
         else
         {
             rb.MovePosition(rb.position + moveVec * CharMove_Speed * Time.deltaTime);
+        }
+    }
+
+    [SerializeField] float TeleportDistance;
+    [SerializeField] float TeleportCoolTime;
+    [SerializeField] float Count;
+    bool doTeleport , once;
+
+    private void FrontTeleport()
+    {
+        if (InputSpaceBar == true && doTeleport == false && CheakMoveFloat > 0)
+        {
+            doTeleport = true;
+            anim.SetTrigger("Tel");
+            rb.position = rb.position + moveVec * TeleportDistance;
+        }
+
+        if (doTeleport)
+        {
+            if (Count != TeleportCoolTime && once == false)
+            {
+                once = true;
+                Count = TeleportCoolTime;
+            }
+
+            Count -= Time.deltaTime;
+
+            if (Count <= 0)
+            {
+                doTeleport = false;
+                once = false;
+
+            }
+
         }
     }
 }
