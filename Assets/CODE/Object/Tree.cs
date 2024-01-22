7c00d71e14713706e6a7776ba12a52d41f088f56
ge15\User <humi2001@nate.com>
2024-01-22 17:53:07 +0900

    < 24 / 01 / 22 > Update Note 1. Tree boneAnimation 구현중

---
+++ Assets/CODE/Object/Tree.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    ParticleSystem leafPs;
+    Animator anim;
+
+    void Start()
+    {
+        leafPs = transform.parent.Find("Leaf").GetComponent<ParticleSystem>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    bool waitPs;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && collision.transform.position.y >= transform.position.y + 1.5f && !waitPs)
+        {
+            waitPs = true;
+            anim.SetTrigger("Touch");
+            Invoke("LeafPs", 0.2f);
+        }
+
+        if (collision.CompareTag("Enemy") && collision.transform.position.y >= transform.position.y + 1.5f && !waitPs)
+        {
+            waitPs = true;
+            anim.SetTrigger("Touch");
+            Invoke("LeafPs", 0.2f);
+        }
+    }
+
+    private void LeafPs()
+    {
+        leafPs.Play();
+    }
+
+    public void A_waitPsFals()
+    {
+        waitPs = false;
+    }
+}
+
+
